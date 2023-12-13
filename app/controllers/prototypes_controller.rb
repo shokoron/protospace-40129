ad90acb8c479cf748ac7a29b4425a99b046dc47b
shokoron <shokoron2011@gmail.com>
2023-12-13 11:43:56 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,68 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_prototype, only: [:edit, :update, :destroy]
+  before_action :check_user_authorization, only: [:edit, :update, :destroy]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to '/'
+    else 
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comments = @prototype.comments.includes(:user)
+    @comment = Comment.new
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+      redirect_to root_path
+  end
+
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def check_user_authorization
+    unless current_user == @prototype.user
+      redirect_to root_path
+    end
+  end
+end
+
+
+
+
+
